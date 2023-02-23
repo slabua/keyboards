1e15d12f22212665e533a3634ec5c55c472101f1
Salvatore La Bua <slabua@gmail.com>
2023-02-23 22:41:51 +0900

    Cleanup

--- jeegva44ergozero/src/R/kb.py
+++ jeegva44ergozero/src/R/kb.py
@@ -19,18 +19,6 @@ class KMKKeyboard(_KMKKeyboard):
     diode_orientation = DiodeOrientation.COL2ROW
 
     # flake8: noqa
-    # coord_mapping = [
-    #     0,  1,  2,  3,  4,  5,
-    #     6,  7,  8,  9, 10, 11,
-    #     12, 13, 14, 15, 16, 17,
-    #     18, 19, 20, 21
-    # ]
-    # coord_mapping = [
-    #     24, 25, 26, 27, 28, 29,
-    #     30, 31, 32, 33, 34, 35,
-    #     36, 37, 38, 39, 40, 41,
-    #     42, 43, 44, 45
-    # ]
     coord_mapping = [
         0,   1,  2,  3,  4,  5, 24, 25, 26, 27, 28, 29,
         6,   7,  8,  9, 10, 11, 30, 31, 32, 33, 34, 35,
